5a67317dffdf70d6d9fb4b7f2545719ff139be4c
Thomas Jetzinger <thomas@jetzinger.com>
2012-09-23 00:43:03 +0200

    Added django-debug-toolbar

--- project_name/settings.py
+++ project_name/settings.py
@@ -114,6 +114,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'lfs.utils.middleware.RedirectFallbackMiddleware',
     "pagination.middleware.PaginationMiddleware",
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = "{{ project_name }}.urls"
@@ -180,6 +181,7 @@ INSTALLED_APPS = (
     'postal',
     'paypal.standard.ipn',
     'paypal.standard.pdt',
+    'debug_toolbar',
 )
 
 FIXTURE_DIRS = (
@@ -198,6 +200,18 @@ AUTHENTICATION_BACKENDS = (
 # For sql_queries
 INTERNAL_IPS = (
     "127.0.0.1",
+)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
 )
 
 CACHE_MIDDLEWARE_KEY_PREFIX = "lfs"
