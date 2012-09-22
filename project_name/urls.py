8ca5ffa5867fef2779fd3e001d0534b67c278a4e
Thomas Jetzinger <thomas@jetzinger.com>
2012-09-22 22:29:37 +0200

    Urlpattern for media only in debug mode

--- project_name/urls.py
+++ project_name/urls.py
@@ -27,4 +27,5 @@ urlpatterns += patterns("",
     (r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
